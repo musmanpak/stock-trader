3495b2c46d39f6dd959ac6d4838eccf25a533c5b
Muhamamd Usman <musman.pak0@gmail.com>
2017-10-14 20:22:12 +0500

    final commit, application completed

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -32,8 +32,8 @@
                            aria-expanded="false"> Save & Load <span class="caret"></span></a>
 
                         <ul class="dropdown-menu">
-                            <li><a href="#">Save Data</a></li>
-                            <li><a href="#">Load Data</a></li>
+                            <li><a href="#" @click="saveData">Save Data</a></li>
+                            <li><a href="#" @click="loadData">Load Data</a></li>
                         </ul>
 
                     </li>
@@ -58,11 +58,24 @@
             }
         },
         methods : {
-            ...mapActions([
-                'randomizeStocks'
-            ]),
+            ...mapActions({
+                randomizeStocks : 'randomizeStocks',
+                fetchData: 'loadData'
+            }),
             endDay() {
                 this.randomizeStocks();
+            },
+            saveData() {
+                const data = {
+                    funds: this.$store.getters.funds,
+                    stockPortfolio: this.$store.getters.stockPortfolio,
+                    stocks: this.$store.getters.stocks
+                };
+
+                this.$http.put('data.json', data);
+            },
+            loadData() {
+                this.fetchData();
             }
         }
     }
--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
+import VueResource from 'vue-resource';
 
 import { routes } from './routes'
 import  store  from './store/store';
 
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://stock-trader-vuejs-26259.firebaseio.com/';
 
 Vue.filter('currency', (value) => {
     return 'Rs. ' + value.toLocaleString();
